0606e4fa4245cff0fdaaf1ff2a62e672f0d831b9
El Rowan <72898533+RowanFromBJC@users.noreply.github.com>
2021-01-23 16:57:13 -0800

    Update dev-lock.js

--- bin/dev-lock.js
+++ bin/dev-lock.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require("fs");
 var lock = fs.readFileSync("./brew.lock", "utf8");
-var arg = ' ' + process.argv[3];
+var arg = process.argv[3] + '\n';
 lock = lock.replace(arg, '');
 fs.writeFile("./brew.lock", lock, (err) => {
 	if (err) throw err;
