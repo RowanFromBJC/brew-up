8864161e695260381ec75dd2789be6f697e3b4dd
El Rowan <72898533+RowanFromBJC@users.noreply.github.com>
2021-02-08 11:27:03 -0800

    Update dev-tap.js

--- bin/dev-tap.js
+++ bin/dev-tap.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 var fs = require('fs');
 var tap = fs.readFileSync('./brew.tap.lock', 'utf8');
-tap = tap.replace(process.argv[3] + '\n', '');
+tap = tap.replace(process.argv[3] + ' ', '');
 fs.writeFile('./brew.tap.lock', tap, (err) => {
 	if (err) throw err;
 });
